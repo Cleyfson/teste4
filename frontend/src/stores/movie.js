e418f42ac8099787ce1c3a106853d01aa5130323
Iago Rios <63796833+iago-r@users.noreply.github.com>
2025-09-20 09:10:00 +0000

    feat(front): add get-genres action

--- frontend/src/stores/movie.js
+++ frontend/src/stores/movie.js
@@ -5,7 +5,17 @@ import { useToast } from '@/composables/useToast';
 export const useMovieStore = defineStore('movies', {
   state: () => ({
     movies: [],
+    genres: [],
   }),
+
+  getters: {
+    genreMap(state) {
+      return state.genres.reduce((map, genre) => {
+        map[genre.id] = genre.name;
+        return map;
+      }, {});
+    },
+  },
 
   actions: {
     async fetchMovies(searchTerm = 'batman') {
@@ -34,6 +44,19 @@ export const useMovieStore = defineStore('movies', {
         return response.data;
       } catch (error) {
         notifyError('Erro ao buscar filme:' + (error.response?.data?.message || error));
+      }
+    },
+
+    async fetchGenres() {
+      const { api } = useApi();
+      const { notifyError } = useToast();
+      
+      try {
+        const response = await api.get('/movies/genres');
+
+        this.genres = response.data;
+      } catch (error) {
+        notifyError('Erro ao buscar gêneros:', error);
       }
     },
   },
